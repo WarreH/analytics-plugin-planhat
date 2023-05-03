2e4dc59c006e3dbc67544ec18fb8dcaf8da0d5d7
WarreH <warrehofmans@gmail.com>
2023-05-03 19:01:00 +0200

    changed: updated examples

--- examples/node/modules/index.js
+++ examples/node/modules/index.js
@@ -8,16 +8,15 @@ app.get("/", (req, res) => {
         count: 1,
         weight: 1,
     });
-    console.log("track");
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 
-    analytics.identify("testUser4", {
-        firstName: "demo",
-        lastName: "user2",
-        email: "test3@test.com",
+    analytics.identify("user12569test", {
+        firstName: "warre",
+        lastName: "hofmans",
+        email: "test@test.com",
         companyExternalId: "testid123456",
     });
 });
